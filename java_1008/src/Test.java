8946734916f941ccd72336df8b1b8988b9c150ac
chenzhe2662 <2291878954@qq.com>
2019-10-18 11:32:43 +0800

    java

---
+++ java_1008/src/Test.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Test {
+    public static void main(String[] args) {
+        //java.util.Date date=new java.util.Date();
+        Date date = new Date();
+        System.out.println(date.getTime());
+    }
+}
